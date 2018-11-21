0f1848e6b5c1711727a513153847a19ecf6ee0e0
lokiteitor <lokiteitor513@gmail.com>
2018-11-21 01:37:17 -0600

    Fix factory fecha

--- api/database/factories/Registro_comida.php
+++ api/database/factories/Registro_comida.php
@@ -3,8 +3,6 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Registro_comida::class, function (Faker $faker) {
-    return [
-        //
-        'fecha' => $faker->dateTimeThisYear()
+    return [    
     ];
 });
