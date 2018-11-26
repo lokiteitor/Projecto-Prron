abc6d2854be037189985d65df2b68dba4596528b
Sleepwalker18 <isradarrellgdz@gmail.com>
2018-11-26 01:43:13 -0600

    reportes a medias

--- api/resources/assets/js/app.js
+++ api/resources/assets/js/app.js
@@ -14,7 +14,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import Login from './views/Login'
+import Mensual from './views/Mensual'
 import App from './views/App'
 import Register from './views/Register'
 import Menu from './views/FoodDescription'
@@ -47,14 +47,14 @@ const router = new VueRouter({
                 title: 'Registro | Empleado'
             }
         },
-        // {
-        //     path: '/Ingresar',
-        //     name: 'login',
-        //     component: Login,
-        //     meta: {
-        //         title: 'Ingresar | Enterprise'
-        //     }
-        // }
+        {
+            path: '/Reportes',
+            name: 'reportes',
+            component: Mensual,
+            meta: {
+                title: 'Reportes | Enterprise'
+            }
+        }
     ],
 });
 
